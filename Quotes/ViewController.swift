4eae2148f98498c640574231c9551f300f8ca02a
Burak Köse <iosdeveloper19@hotmail.com>
2022-07-05 22:49:10 +0300

    Initial Commit

---
+++ Quotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quotes
+//
+//  Created by Burak Köse on 5.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
